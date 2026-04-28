a022404a29c2220fb03bf8677cf7f837d16cbe75
Kevin Moenga <kmoenga2@gmail.com>
2026-04-29 00:10:12 +0300

    fix Vercel deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      phaser: 'phaser/dist/phaser.js',
-    },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
   },
 })
